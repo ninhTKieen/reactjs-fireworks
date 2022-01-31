20c31fe19b277fc534ba4d14d0397e9e83aa17ce
ninhTKieen <ninhtrungkien2811@gmail.com>
2022-01-31 23:55:20 +0700

    change background

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import Countdown from "src/components/Countdown";
 import Firework from "src/components/Firework";
+import skyBackground from "src/assets/sky.jpeg";
 
 const App = () => {
   const [countdown, setCountdown] = useState(true);
   const [time, setTime] = useState("");
 
   return (
-    <div className="container">
+    <div
+      className="container"
+      style={{
+        backgroundImage: `url(${skyBackground})`,
+        width: "100%",
+      }}
+    >
       {countdown ? (
         <Countdown
           countdown={countdown}
